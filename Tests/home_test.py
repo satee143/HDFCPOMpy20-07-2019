0018310ff1629e8e9e8eb828de6bb23f2720073a
satheesh <satee143@gmail.com>
2019-07-19 12:54:45 +0530

    19-07-2019

--- Tests/home_test.py
+++ Tests/home_test.py
@@ -6,27 +6,40 @@ from Pages.Struct_content_Details import Struct_Content_Details
 from Pages.Customize_Quote import Customize_Quote
 from Pages.Fill_Details import Fill_Details
 from Utils import util as utils
-#from Utils import readexcel
 import time
 
 
 
 @pytest.mark.usefixtures('Setup')
-
 class Test_Home_selc():
 
+    # Home insurance quote for structure
     def test_owner_quote_structure(self):
+
+        # Calling Driver
         driver=self.driver
+
+        # Launching HDFC ERGO Home Page
         driver.get(utils.url)
+
+        # Selecting Home Insurance
         Hdfchome=hdfchomepage(driver)
         Hdfchome.selecting_homeinsurance()
+
+        # Selecting Buy Now Button
         Home_insur=home_insurance_homepage(driver)
         Home_insur.Click_on_Buy_Now()
+
+        # Switching from Current window to Quote Window
         driver.switch_to.window(driver.window_handles[1])
+
+        # Selecting Insurance type
         coverpage=home_insurance_coverage(driver)
         coverpage.Selection_type()
         coverpage.Selcetion_struct_type()
         coverpage.Click_On_Continue()
+
+        # Selecting  Structure Type and etc...
         Struct_Content=Struct_Content_Details(driver)
         time.sleep(5)
         Struct_Content.Select_Valuation(utils.value)
@@ -36,6 +49,10 @@ class Test_Home_selc():
         Struct_Content.Click_Next_Button()
         Struct_Content.Entering_Basic_Details(utils.name,utils.email,utils.mobile)
 
+
+
+
+    # Renter Quote for Content value
     def test_tenant_quote_content(self):
         driver=self.driver
         driver.get(utils.url)
@@ -57,7 +74,7 @@ class Test_Home_selc():
         Struct_Content.Click_Next_Button_Tenant()
         Struct_Content.Entering_Basic_Details(utils.name,utils.email,utils.mobile)
 
-
+    # Home insurance quote for structure and Content(Both) value
     def test_owner_quote_structure_content(self):
         driver=self.driver
         driver.get(utils.url)
@@ -80,7 +97,7 @@ class Test_Home_selc():
         Struct_Content.Click_Next_Button()
         Struct_Content.Entering_Basic_Details(utils.name,utils.email,utils.mobile)
 
-
+    # Additional coverages to Home insurance quote for structure
     def test_add_additional_coverages(self):
         driver = self.driver
         driver.get(utils.url)
@@ -106,6 +123,8 @@ class Test_Home_selc():
         CustomizeQuote.Select_Additonal_Coverage(utils.bicycle_coverage)
         CustomizeQuote.Select_Quote()
 
+
+    # Add Co-owner to Home insurance quote for structure
     def test_add_coowner(self):
         driver = self.driver
         driver.get(utils.url)
@@ -136,6 +155,8 @@ class Test_Home_selc():
         filldetails.Select_Coowner(utils.coowner)
         filldetails.Select_Address(utils.dob,utils.pincode,utils.h_no,utils.street)
 
+
+    # Correspondence address different
     def test_correspondance_address(self):
         driver = self.driver
         driver.get(utils.url)
@@ -168,7 +189,7 @@ class Test_Home_selc():
         filldetails.Select_Correspondance_Address(utils.cpincode,utils.chno,utils.cstreet)
         filldetails.Select_Next_Button()
 
-
+    #Add financier details
     def test_add_fianacier(self):
         driver = self.driver
         driver.get(utils.url)
@@ -186,7 +207,7 @@ class Test_Home_selc():
         Struct_Content.Select_Valuation(utils.value)
         Struct_Content.Builtup_Area(utils.area)
         Struct_Content.Select_AgeofProp_PropType()
-        Struct_Content.Select_Security_Measures()
+        #Struct_Content.Select_Security_Measures()
         Struct_Content.Click_Next_Button()
         Struct_Content.Entering_Basic_Details(utils.name, utils.email, utils.mobile)
         time.sleep(4)
@@ -196,8 +217,8 @@ class Test_Home_selc():
         CustomizeQuote.Select_Quote()
         filldetails = Fill_Details(driver)
         time.sleep(3)
-        filldetails.Select_Coowner(utils.coowner)
+        #filldetails.Select_Coowner(utils.coowner)
         filldetails.Select_Address(utils.dob, utils.pincode, utils.h_no, utils.street)
-        filldetails.Select_Correspondance_Address(utils.cpincode,utils.chno,utils.cstreet)
+        #filldetails.Select_Correspondance_Address(utils.cpincode,utils.chno,utils.cstreet)
         filldetails.Select_fiancier(utils.financier)
         filldetails.Select_Next_Button()
--- conftest.py
+++ conftest.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import IEDriverManager
+from webdriver_manager.microsoft import EdgeDriverManager
 import pytest
 import time
 
@@ -12,10 +14,23 @@ def pytest_addoption(parser):
 def Setup(request):
     try:
         browser=request.config.getoption('--browser')
+
         if browser=='chrome':
             driver=webdriver.Chrome(ChromeDriverManager().install())
+
+
         elif browser=='firefox':
             driver=webdriver.Firefox(executable_path=GeckoDriverManager().install())
+
+
+        elif browser== ('ie' or  'internet explorer'):
+            driver=webdriver.Ie(IEDriverManager().install())
+
+
+        elif browser== 'edge':
+            driver=webdriver.Edge(EdgeDriverManager().install())
+
+
         driver.maximize_window()
         request.cls.driver=driver
         yield
